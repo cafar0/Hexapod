bea56c2ae0d1562399a4e62a9cb46c6a10764494
I333657 <calin.rodina@sap.com>
2017-03-23 13:32:24 +0200

    Added functionality to forawrd.sh

--- forward.sh
+++ forward.sh
@@ -1,3 +1,37 @@
 #!/bin/bash
-sleep 5s
-/bin/bash ./tripod_move_tibia.sh "right" "0.0"
+
+SLEEP_PERIOD=3
+
+# Set initial position
+sleep $SLEEP_PERIODs
+/bin/bash ./tripod_move_tibia.sh "right" "0.0" & ./tripod_move_tibia "left" "0.0"
+
+sleep $SLEEP_PERIODs
+/bin/bash ./tripod_move_femur.sh "right" "0.0" & ./tripod_move_femur "left" "0.0"
+
+# Prepare for Right Step
+sleep $SLEEP_PERIODs
+/bin/bash ./tripod_move_femur.sh "right" "1.0" 
+
+
+ while [  1  ]; do
+    # Right step
+	sleep $SLEEP_PERIODs
+	/bin/bash ./tripod_move_coxa.sh "right" "0.4"
+
+	sleep $SLEEP_PERIODs
+	/bin/bash ./tripod_move_femur.sh "right" "0.0"
+
+	sleep $SLEEP_PERIODs
+	/bin/bash ./tripod_move_coxa.sh "right" "0.0" & ./tripod_move_femur.sh "left" "1.0"
+
+	# Left step
+	sleep $SLEEP_PERIODs
+	/bin/bash ./tripod_move_coxa.sh "left" "-0.4"
+
+	sleep $SLEEP_PERIODs
+	/bin/bash ./tripod_move_femur.sh "left" "0.0"
+
+	sleep $SLEEP_PERIODs
+	/bin/bash ./tripod_move_coxa.sh "left" "0.0" & ./tripod_move_femur.sh "right" "1.0"
+ done
