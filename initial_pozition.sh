b42b69fcad50efe9b0a70d3849705e4a06309033
calin10 <calin_rodina@yahoo.com>
2017-03-27 21:47:30 +0300

    Added script for initial position

---
+++ initial_pozition.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+/bin/bash ./tripod_move_tibia.sh "right" "0.0" & 
+          ./tripod_move_tibia.sh "left"  "0.0" &
+          ./tripod_move_femur.sh "right" "0.0" & 
+          ./tripod_move_femur.sh "left"  "0.0"
