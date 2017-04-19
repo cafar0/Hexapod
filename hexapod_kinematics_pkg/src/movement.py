437bc5b261b7836d19b0df12a3937d2f0ed4ce5d
calin10 <calin_rodina@yahoo.com>
2017-04-19 21:16:27 +0300

    small step improvement

--- hexapod_kinematics_pkg/src/movement.py
+++ hexapod_kinematics_pkg/src/movement.py
@@ -5,7 +5,7 @@ from  std_msgs.msg import  Float64
 
 FEMUR_UP = 1.0
 COXA_FWD = 0.4
-DOWN = 0.0
+DOWN = 0.1
 NEUTRAL = 0.0
 
 class Movement(): 
@@ -43,30 +43,45 @@ class Movement():
         self.pub_coxa_r2 = coxa_right[1]
         self.pub_coxa_r3 = coxa_right[2]
 
+    
+    def moveLeftLeg(self,value) : 
+        self.pub_femur_l1.publish(value)
+        self.pub_femur_r2.publish(value)
+        self.pub_femur_l3.publish(value)
+
+    def moveRightLeg(self,value) :
+        self.pub_femur_r1.publish(value)
+        self.pub_femur_l2.publish(value)
+        self.pub_femur_r3.publish(value)
+
     def liftLeftLeg(self) :
-        self.pub_femur_l1.publish(FEMUR_UP)
         self.pub_femur_r2.publish(FEMUR_UP)
+        self.pub_femur_l1.publish(FEMUR_UP)
         self.pub_femur_l3.publish(FEMUR_UP)
+        self.moveRightLeg(-0.2)
 
     def lowerLeftLeg(self) :
         self.pub_femur_l1.publish(DOWN)
         self.pub_femur_r2.publish(DOWN)
         self.pub_femur_l3.publish(DOWN)
+        # self.moveRightLeg(0.0)
 
     def liftRightLeg(self) :
-        self.pub_femur_r1.publish(FEMUR_UP)
         self.pub_femur_l2.publish(FEMUR_UP)
         self.pub_femur_r3.publish(FEMUR_UP)
+        self.pub_femur_r1.publish(FEMUR_UP)
+        self.moveLeftLeg(-0.2)
 
     def lowerRightLeg(self) :
         self.pub_femur_r1.publish(DOWN)
         self.pub_femur_l2.publish(DOWN)
         self.pub_femur_r3.publish(DOWN)
+        # self.moveLeftLeg(0.0)
 
     def moveLeftLegForward(self) :
-        self.pub_coxa_l1.publish(0.4)
-        self.pub_coxa_r2.publish(-0.4)
-        self.pub_coxa_l3.publish(0.4)
+        self.pub_coxa_l1.publish(COXA_FWD)
+        self.pub_coxa_r2.publish(-COXA_FWD)
+        self.pub_coxa_l3.publish(COXA_FWD)
 
     def moveLeftLegNeutral(self) :
         self.pub_coxa_l1.publish(NEUTRAL)
